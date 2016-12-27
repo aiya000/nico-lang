5d595414a792ba32a840f08635d2e03a3079a6f2
aiya000 <aiya000.develop@gmail.com>
2016-12-27 22:44:55 +0900

    Create nico-chan

---
+++ README.md
@@ -0,0 +1,2 @@
+# Nico-lang
+Nico-lang is the programing language of Nico Yazawa.
---
+++ src/Main.hs
@@ -0,0 +1,6 @@
+-- | The entry point of the program
+module Main where
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
