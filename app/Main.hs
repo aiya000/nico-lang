5ff0bc59147d4eb5a2fa525ff37d2ed575130b23
aiya000 <aiya000.develop@gmail.com>
2016-12-28 20:49:41 +0900

    Update app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 -- | The entry point of the program
 module Main where
 
+import System.Environment (getArgs)
+import qualified Data.Text as T
+import qualified NicoLang.Parser as NicoParser
+
+
+-- | The entry point of the program
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  --TODO: implement some options
+  nicoFile <- head <$> getArgs
+  nicoCode <- T.pack <$> readFile nicoFile
+  let nicoAbstract = NicoParser.parse nicoCode
+  --TODO: Do evaluate
+  print nicoAbstract
