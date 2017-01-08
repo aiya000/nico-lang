39355fd4db22f132cad7e03ae8d8baf0ac2b5a3d
aiya000 <aiya000.develop@gmail.com>
2017-01-08 17:19:20 +0900

    Add getInOutTests to test/Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,57 @@
-import Test.Tasty
+{-# LANGUAGE LambdaCase #-}
+
+import Control.Monad (forM)
+import Control.Monad.Trans.State.Lazy (runStateT)
+import Data.Bifunctor (first)
+import Data.List (nub)
+import Data.Tuple.Extra ((&&&))
+import NicoLang.Evaluator (emptyMachine, eval)
+import NicoLang.Parser (parse)
+import System.EasyFile (getDirectoryContents)
+import System.IO.Silently (capture_)
+import Test.Tasty (TestTree)
+import Test.Tasty.HUnit ((@?=))
+import Text.Printf (printf)
+import qualified Data.Text as T
 import qualified NicoLangTest.ParserTest as PT
+import qualified Test.Tasty as Test (defaultMain, testGroup)
+import qualified Test.Tasty.HUnit as Test (testCase, assertFailure)
 
 
 main :: IO ()
-main = defaultMain $
-  testGroup "nico-lang test"
-    [ PT.test
-    ]
+main = do
+  inOutTests <- getInOutTests
+  Test.defaultMain $
+    Test.testGroup "nico-lang test" $
+      [ PT.test
+      , inOutTests
+      ]
+
+getInOutTests :: IO TestTree
+getInOutTests = do
+  inOutPairs  <- map inOutFiles . testNames . filter (`notElem` [".", ".."]) <$> getDirectoryContents "test/in-out"
+  inOutPairs' <- sequence . map (twiceMapM readFile) $ inOutPairs
+  resultPairs <- forM inOutPairs' $ firstMapM $ \source -> do
+    case parse . T.pack $ source of
+      Left  e -> return . Left $ "Parse error: " ++ e
+      Right a -> return . Right =<< (capture_ . flip runStateT emptyMachine $ eval a)
+  return $ Test.testGroup "in-out matching test" $
+    flip map (zip [1..] resultPairs) $ \case
+      (i, (Left e, _))             -> Test.testCase (show i) $ Test.assertFailure e
+      (i, (Right inData, outData)) -> Test.testCase (show i) $ inData @?= outData
+  where
+    testNames :: [FilePath] -> [FilePath]
+    testNames  = nub . map (takeWhile (/='.'))
+    inOutFiles :: FilePath -> (FilePath, FilePath)
+    inOutFiles = (printf "test/in-out/%s.nico") &&& (printf "test/in-out/%s.out")
+
+twiceMapM :: Monad m => (a -> m b) -> (a, a) -> m (b, b)
+twiceMapM f (x, y) = do
+  x' <- f x
+  y' <- f y
+  return (x', y')
+
+firstMapM :: Monad m => (a -> m b) -> (a, x) -> m (b, x)
+firstMapM f (x, y) = do
+  x' <- f x
+  return (x', y)
