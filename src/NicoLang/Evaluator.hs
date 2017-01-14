678fe8c3ccdde7e4b08080120c061a9f5ab1764e
aiya000 <aiya000.develop@gmail.com>
2017-01-14 15:01:29 +0900

    Change NicoMachine definition

--- src/NicoLang/Evaluator.hs
+++ src/NicoLang/Evaluator.hs
@@ -9,36 +9,45 @@ module NicoLang.Evaluator
 
 import Control.Monad.Trans.State.Lazy (StateT, get, put)
 import Data.IntMap.Lazy (IntMap)
+import Data.Stack (Stack, push, pop, runStack, stack)
 import NicoLang.Parser.Items
 import qualified Data.IntMap.Lazy as M
 
 -- | The virtual machine's memory
 type NicoMemory = IntMap Int
 
--- | The state of current pointer
+-- | The state of the current pointer
 type NicoPointer = Int
 
 -- | The virtual machine's state
-type NicoMachine = (NicoMemory, NicoPointer)
+data NicoMachine = NicoMachine
+  { nicoMemory            :: NicoMemory  -- ^ The state of the NicoProgram result
+  , nicoPointer           :: NicoPointer  -- ^ The NicoProgram's current cell
+  , nicoLoopBeginPointers :: Stack NicoPointer NicoPointer  -- ^ The NicoPointer is pushed to here when The NicoLoopBegin was evaluated
+  }
 
 
 -- | The initial state of NicoMachine
 emptyMachine :: NicoMachine
-emptyMachine = (M.empty, 0)
+emptyMachine = let illigalPointer = (-1)
+                   emptyStack     = stack $ \xs -> (illigalPointer, [])
+                   startPoint     = 0
+               in NicoMachine M.empty startPoint emptyStack
 
 
 -- | Evaluate and execute NicoLangAbstractSyntaxList with the virtual machine state
 eval :: NicoLangAbstractSyntaxList -> StateT NicoMachine IO NicoMemory
 eval [] = do
-  (mem, _) <- get
+  (NicoMachine mem _ _) <- get
   return mem
 
 eval (NicoForward:rest) = do
-  (mem, p) <- get
-  case M.lookup p mem of
-    Nothing  -> put $ (M.insert p 1 mem, p)  -- 1 is `0 (initial state) + 1 (forward)`
-    Just val -> let newVal = val + 1
-                in put $ (M.insert p newVal mem, p)  -- Replace val to newVal on mem
+  (NicoMachine mem np st) <- get
+  case M.lookup np mem of
+    Nothing  -> let newMem = M.insert np 1 mem  -- 1 is `0 (initial state) + 1 (forward)`
+                in put $ NicoMachine newMem np st
+    Just val -> let newMem = M.insert np (val + 1) mem  -- Replace val to `val + 1` on mem
+                in put $ NicoMachine newMem np st
   return mem
 
 eval (NicoBackword:rest)  = undefined
