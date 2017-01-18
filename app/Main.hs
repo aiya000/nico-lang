c313f20d0aa692097f7c4ad1c3703e8ee2852cb9
aiya000 <aiya000.develop@gmail.com>
2017-01-19 00:11:02 +0900

    Move Main.main to NicoLang.Main.defaultMain

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,6 @@
--- | The entry point of the program
 module Main where
 
-import CmdOptions (NicoRunOptions(nicoRunTargetSourceFile, nicoRunTransToBF, nicoRunDebug, nicoRunShowResultMemory), nicoRunOptions)
-import Control.Monad (mapM_)
-import NicoLang.Evaluator (emptyMachine, eval, runNicoState)
-import NicoLang.Parser (parse)
-import System.Console.CmdArgs (cmdArgs)
-import qualified Data.Text as T
+import NicoLang.Main (defaultMain)
 
-
--- | The entry point of the program
 main :: IO ()
-main = do
-  options <- cmdArgs nicoRunOptions
-  case nicoRunTargetSourceFile options of
-    Nothing       -> error "Please specify the source code"
-    Just nicoFile -> do
-      nicoCode <- T.pack <$> readFile nicoFile
-      case parse nicoCode of
-        Left  e -> error $ "Caught the error: " ++ e
-        Right a -> if nicoRunTransToBF options
-          then mapM_ (putStr . show) a >> putStrLn ""
-          else do
-            (results, _) <- flip runNicoState emptyMachine $ eval a
-            branch results options
-  where
-    branch results@(_, logs) opts | nicoRunDebug opts = do
-      putStrLn ""
-      mapM_ putStrLn logs
-      branch results (opts { nicoRunDebug = False })
-
-    branch results@(mem, _) opts | nicoRunShowResultMemory opts = do
-      putStrLn ""
-      print mem
-      branch results (opts { nicoRunShowResultMemory = False })
-
-    -- The end of branches
-    branch _ _ = return ()
+main = defaultMain
---
+++ src/NicoLang/Main.hs
@@ -0,0 +1,39 @@
+-- | The entry point of the program
+module NicoLang.Main (defaultMain) where
+
+import Control.Monad (mapM_)
+import NicoLang.CliOptions (NicoRunOptions(nicoRunTargetSourceFile, nicoRunTransToBF, nicoRunDebug, nicoRunShowResultMemory), nicoRunOptions)
+import NicoLang.Evaluator (emptyMachine, eval, runNicoState)
+import NicoLang.Parser (parse)
+import System.Console.CmdArgs (cmdArgs)
+import qualified Data.Text as T
+
+
+-- | The entry point of the program
+defaultMain :: IO ()
+defaultMain = do
+  options <- cmdArgs nicoRunOptions
+  case nicoRunTargetSourceFile options of
+    Nothing       -> error "Please specify the source code"
+    Just nicoFile -> do
+      nicoCode <- T.pack <$> readFile nicoFile
+      case parse nicoCode of
+        Left  e -> error $ "Caught the error: " ++ e
+        Right a -> if nicoRunTransToBF options
+          then mapM_ (putStr . show) a >> putStrLn ""
+          else do
+            (results, _) <- flip runNicoState emptyMachine $ eval a
+            executeOptions results options
+  where
+    executeOptions results@(_, logs) opts | nicoRunDebug opts = do
+      putStrLn ""
+      mapM_ putStrLn logs
+      executeOptions results (opts { nicoRunDebug = False })
+
+    executeOptions results@(mem, _) opts | nicoRunShowResultMemory opts = do
+      putStrLn ""
+      print mem
+      executeOptions results (opts { nicoRunShowResultMemory = False })
+
+    -- The end of branches
+    executeOptions _ _ = return ()
