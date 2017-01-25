4fd0f9038c49d5643414d447c6bd17890683d756
aiya000 <aiya000.develop@gmail.com>
2017-01-25 15:07:39 +0900

    Add comments to parser combinators

--- src/NicoLang/Parser.hs
+++ src/NicoLang/Parser.hs
@@ -46,26 +46,34 @@ tokenTexts = [ nicoForwardText
              , nicoLoopEndText
              ]
 
+-- Parse a code to [NicoOperation]
 codeParser :: Parser (Maybe [NicoOperation])
 codeParser = do
   tokens <- tokensParser
   let mayOperations = mapM (flip M.lookup operationMap) tokens
   return mayOperations
 
+-- Parse a code to tokens
 tokensParser :: Parser [Text]
 tokensParser = do
   blocks <- P.many tokenBlockParser
   let tokens = concat blocks
   return tokens
 
+-- A block means "あああ にっこにっこにーにこにーって覚えてラブニコ！ いいい" .
+--                       |^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|
+-- And parse its text to [にっこにっこにー, にこにーって覚えてラブニコ！]
 tokenBlockParser :: Parser [Text]
 tokenBlockParser = do
   skipNonToken
   P.many tokenParser
 
+-- Match any token
 tokenParser :: Parser Text
 tokenParser = foldl1' (<|>) . map (P.try . P.string) $ tokenTexts
 
+-- Skip other than the token
+-- (Seek to just before the token)
 skipNonToken :: Parser ()
 skipNonToken = do
   let heads = map T.head tokenTexts
