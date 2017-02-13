36b3422cf7d786a51e4910a89c6ace8d66f1ef0b
aiya000 <aiya000.develop@gmail.com>
2017-02-13 12:38:27 +0900

    Remove unneeded test

--- test/NicoLangTest/ParserTest.hs
+++ test/NicoLangTest/ParserTest.hs
@@ -2,22 +2,24 @@
 
 module NicoLangTest.ParserTest where
 
+import Brainhack.Parser.Items (BrainfuckOperator (..))
 import Control.Exception.Safe (SomeException)
 import Data.Text (Text)
 import NicoLang.Parser.Items
+import NicoLang.Parser.Items (NicoToken)
 import Test.Tasty
 import Test.Tasty.HUnit
 import qualified Brainhack.Parser as Parser
 import qualified Data.Text as T
 
 
-simplySourceCode :: Text
+simplySourceCode :: NicoToken
 simplySourceCode = "笑顔届ける矢澤にこにこ！にっこにっこにーにっこにっこにーにこにーって覚えてラブニコ！にこにーって覚えてラブニコ！だめだめだめっ！あなたのハートににこにこにー！ぴょんぴょんぴょんっ！"
 
-simplySourceCodeAbstract :: NicoLangProgram
-simplySourceCodeAbstract = [NicoForward, NicoIncr, NicoIncr, NicoDecr, NicoDecr, NicoBackword, NicoInput, NicoOutput]
+simplySourceCodeAbstract :: [BrainfuckOperator]
+simplySourceCodeAbstract = [ForwardOp, IncrOp, IncrOp, DecrOp, DecrOp, BackwardOp, InputOp, OutputOp]
 
-sourceCode :: Text
+sourceCode :: NicoToken
 sourceCode = "笑顔届ける矢澤にこにこ！にっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにこにーはみんなのもの！だめだめだめっ！にっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにー笑顔届ける矢澤にこにこ！にこにーって覚えてラブニコ！ｷﾓﾁﾜﾙｲだめだめだめっ！ぴょんぴょんぴょんっ！笑顔届ける矢澤にこにこ！笑顔届ける矢澤にこにこ！にっこにっこにーにっこにっこにーにっこにっこにーにこにーはみんなのもの！だめだめだめっ！にっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにー笑顔届ける矢澤にこにこ！にこにーって覚えてラブニコ！ｷﾓﾁﾜﾙｲだめだめだめっ！ぴょんぴょんぴょんっ！笑顔届ける矢澤にこにこ！笑顔届ける矢澤にこにこ！にっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにこにーはみんなのもの！だめだめだめっ！にっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにー笑顔届ける矢澤にこにこ！にこにーって覚えてラブニコ！ｷﾓﾁﾜﾙｲだめだめだめっ！ぴょんぴょんぴょんっ！笑顔届ける矢澤にこにこ！笑顔届ける矢澤にこにこ！にっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにこにーはみんなのもの！だめだめだめっ！にっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにー笑顔届ける矢澤にこにこ！にこにーって覚えてラブニコ！ｷﾓﾁﾜﾙｲだめだめだめっ！ぴょんぴょんぴょんっ！笑顔届ける矢澤にこにこ！笑顔届ける矢澤にこにこ！にっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにこにーはみんなのもの！だめだめだめっ！にっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにーにっこにっこにー笑顔届ける矢澤にこにこ！にこにーって覚えてラブニコ！ｷﾓﾁﾜﾙｲだめだめだめっ！にっこにっこにーぴょんぴょんぴょんっ！笑顔届ける矢澤にこにこ！"
 
 
@@ -25,11 +27,7 @@ test :: TestTree
 test =
   testGroup "Parser test"
     [ testCase "Parse from source code text to nico-lang abstract" $
-        case (Parser.parse simplySourceCode :: Either SomeException NicoLangProgram) of
-          Left _  -> assertFailure "Parsing is fail"
+        case Parser.parse simplySourceCode of
+          Left  _ -> assertFailure "Parsing is fail"
           Right x -> x @?= simplySourceCodeAbstract
-    , testCase "Parse result can be restored by ShowT" $
-        case (Parser.parse sourceCode :: Either SomeException NicoLangProgram) of
-          Left _  -> assertFailure "Parsing is fail"
-          Right x -> (T.concat . map showT $ x) @?= sourceCode
     ]
